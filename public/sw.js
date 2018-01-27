3e7f3420a08fe5a2b18af2dcb2821f9e95823798
Michael Genesini <michael.genesini@gmail.com>
2018-01-27 11:51:20 +0100

    removed shadow ios on tap

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 var serviceWorkerOption = {
   "assets": [
-    "/assets/scripts/0.e0a4.js",
-    "/assets/scripts/main.e0a4.js",
+    "/assets/scripts/0.6a2c.js",
+    "/assets/scripts/main.6a2c.js",
     "/assets/fonts/Bodoni.ttf",
     "/assets/fonts/InputMono-Regular.ttf"
   ]
