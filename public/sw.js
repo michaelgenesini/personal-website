5ef5fc4a05a483c37b4bf5801e76762152a18f7e
Michael Genesini <michael.genesini@gmail.com>
2018-01-27 11:26:37 +0100

    added first text hidden

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 var serviceWorkerOption = {
   "assets": [
-    "/assets/scripts/main.3660.js",
+    "/assets/scripts/main.87c8.js",
     "/assets/fonts/Bodoni.ttf",
     "/assets/fonts/InputMono-Regular.ttf"
   ]
