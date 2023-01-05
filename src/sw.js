ce30faa0bf0aec081027a3b1c6b97baab94e1d18
Michael Genesini <michaelgenesini@MacBook-Pro-di-Mike.local>
2023-01-05 12:08:42 -0100

    initial commit 👨🔬

--- src/sw.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//         // Registration was successful
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }, function(err) {
-//         // registration failed :(
-//         console.log('ServiceWorker registration failed: ', err);
-//     })
-// })
